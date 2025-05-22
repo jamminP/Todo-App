cbefe04f7e72a1e688d09f51d3e4054c95bcf382
jamminP <parkjaemin08@gmail.com>
2025-05-22 12:52:07 +0900

    나만의 Todo App 만들기 박재민

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import "./App.css";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
+    { id: 0, content: "123" , isclear: false, isEditing: false},
+    { id: 1, content: "코딩 공부하기" , isclear: false, isEditing: false},
+    { id: 2, content: "잠 자기" , isclear: false, isEditing: false},
   ]);
 
   return (
-    <>
+    <div className="todo-container">
+      <Header></Header>
       <TodoList todoList={todoList} setTodoList={setTodoList} />
       <hr />
       <TodoInput todoList={todoList} setTodoList={setTodoList} />
+    </div>
+  );
+}
+
+function Header(){
+  return(
+    <>
+      <h1>나만의 Todo App 만들기</h1>
     </>
   );
 }
@@ -28,7 +37,7 @@ function TodoInput({ todoList, setTodoList }) {
       />
       <button
         onClick={() => {
-          const newTodo = { id: Number(new Date()), content: inputValue };
+          const newTodo = { id: Number(new Date()), content: inputValue, isclear: false , isEditing: false};
           const newTodoList = [...todoList, newTodo];
           setTodoList(newTodoList);
           setInputValue("");
@@ -44,42 +53,82 @@ function TodoList({ todoList, setTodoList }) {
   return (
     <ul>
       {todoList.map((todo) => (
-        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
+        <Todo key={todo.id} todo={todo} setTodoList={setTodoList}/>
       ))}
     </ul>
   );
 }
 
-function Todo({ todo, setTodoList }) {
+function Todo({ todo, setTodoList}) {
   const [inputValue, setInputValue] = useState("");
   return (
     <li>
-      {todo.content}
+  <div className="todo-content">
+    <input
+      type="checkbox"
+      checked={todo.isclear}
+      onChange={() => {
+        setTodoList((prev) =>
+          prev.map((el) =>
+            el.id === todo.id ? { ...el, isclear: !el.isclear } : el
+          )
+        );
+      }}
+    />
+    {!todo.isEditing ? (
+      <span className="todo-text" style={{ textDecoration: todo.isclear ? "line-through" : "none" }}>
+        {todo.content}
+      </span>
+    ) : (
       <input
+        type="text"
         value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
+        onChange={(e) => setInputValue(e.target.value)}
       />
+    )}
+  </div>
+
+  <div className="todo-buttons">
+    {!todo.isEditing ? (
       <button
+        className="button-edit"
         onClick={() => {
           setTodoList((prev) =>
             prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
+              el.id === todo.id ? { ...el, isEditing: true } : el
             )
           );
         }}
       >
         수정
       </button>
+    ) : (
       <button
+        className="button-edit"
         onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
+          setTodoList((prev) =>
+            prev.map((el) =>
+              el.id === todo.id
+                ? { ...el, content: inputValue, isEditing: false }
+                : el
+            )
+          );
         }}
       >
-        삭제
+        완료
       </button>
-    </li>
+    )}
+    <button
+      className="button-delete"
+      onClick={() => {
+        setTodoList((prev) => prev.filter((el) => el.id !== todo.id));
+      }}
+    >
+      삭제
+    </button>
+  </div>
+</li>
+
   );
 }
 
